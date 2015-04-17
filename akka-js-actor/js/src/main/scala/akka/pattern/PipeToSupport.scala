f78e5b4506307d1902bf79884ed737455613486f
yawnt <me@yawnt.com>
2015-04-17 11:05:44 +0200

    mv: PipeTo => PipeToSupport

--- akka-js-actor/js/src/main/scala/akka/pattern/PipeToSupport.scala
+++ akka-js-actor/js/src/main/scala/akka/pattern/PipeToSupport.scala
@@ -1,39 +1,41 @@
+/**
+ * Copyright (C) 2009-2014 Typesafe Inc. <http://www.typesafe.com>
+ */
 package akka.pattern
 
 import language.implicitConversions
-
 import scala.concurrent.{ Future, ExecutionContext }
 import scala.util.{ Failure, Success }
-
-import akka.actor._
+import akka.actor.{ Status, ActorRef, Actor }
+import akka.actor.ActorSelection
 
 trait PipeToSupport {
 
   final class PipeableFuture[T](val future: Future[T])(implicit executionContext: ExecutionContext) {
     def pipeTo(recipient: ActorRef)(implicit sender: ActorRef = Actor.noSender): Future[T] = {
       future onComplete {
-        case Success(r) => recipient ! r
-        case Failure(f) => recipient ! Status.Failure(f)
+        case Success(r) ⇒ recipient ! r
+        case Failure(f) ⇒ recipient ! Status.Failure(f)
       }
       future
     }
-    /*def pipeToSelection(recipient: ActorSelection)(implicit sender: ActorRef = Actor.noSender): Future[T] = {
+    def pipeToSelection(recipient: ActorSelection)(implicit sender: ActorRef = Actor.noSender): Future[T] = {
       future onComplete {
-        case Success(r) => recipient ! r
-        case Failure(f) => recipient ! Status.Failure(f)
+        case Success(r) ⇒ recipient ! r
+        case Failure(f) ⇒ recipient ! Status.Failure(f)
       }
       future
-    }*/
+    }
     def to(recipient: ActorRef): PipeableFuture[T] = to(recipient, Actor.noSender)
     def to(recipient: ActorRef, sender: ActorRef): PipeableFuture[T] = {
       pipeTo(recipient)(sender)
       this
     }
-    /*def to(recipient: ActorSelection): PipeableFuture[T] = to(recipient, Actor.noSender)
+    def to(recipient: ActorSelection): PipeableFuture[T] = to(recipient, Actor.noSender)
     def to(recipient: ActorSelection, sender: ActorRef): PipeableFuture[T] = {
       pipeToSelection(recipient)(sender)
       this
-    }*/
+    }
   }
 
   /**
@@ -55,5 +57,3 @@ trait PipeToSupport {
    */
   implicit def pipe[T](future: Future[T])(implicit executionContext: ExecutionContext): PipeableFuture[T] = new PipeableFuture(future)
 }
-
-object PipeTo extends PipeToSupport

ffbd62a8ab01894150d7e37d1be65aa13a3e0b0f
yawnt <me@yawnt.com>
2015-04-22 15:58:19 +0200

    fix: add ExecutorServiceDelegate

---
+++ akka-js-actor/js/src/main/scala/akka/dispatch/ExecutorServiceDelegate.scala
@@ -0,0 +1,40 @@
+package akka.dispatch
+
+import java.util.Collection
+import java.util.concurrent.{ TimeUnit, Callable, ExecutorService }
+import scalajs.js.timers.setTimeout
+
+trait ExecutorServiceDelegate extends ExecutorService {
+  private[this] var _isShutdown = false
+  
+  def execute(command: Runnable) = if (!_isShutdown) setTimeout(0) {
+    command.run()
+  } 
+  
+  def shutdown() = _isShutdown = true
+  
+  def shutdownNow() = ???
+  
+  def isShutdown() = _isShutdown
+
+  def isTerminated = ???
+
+  def awaitTermination(l: Long, timeUnit: TimeUnit) = ???
+
+  def submit[T](callable: Callable[T]) = ???
+
+  def submit[T](runnable: Runnable, t: T) = ???
+
+  def submit(runnable: Runnable) = ???
+
+  def invokeAll[T](callables: Collection[_ <: Callable[T]]) = ???
+
+  def invokeAll[T](callables: Collection[_ <: Callable[T]], l: Long, timeUnit: TimeUnit) = ???
+
+  def invokeAny[T](callables: Collection[_ <: Callable[T]]) = ???
+
+  def invokeAny[T](callables: Collection[_ <: Callable[T]], l: Long, timeUnit: TimeUnit) = ???
+
+  
+  
+}

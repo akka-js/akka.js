1e66d2042823eb88917dfe5fdf9956a270724acc
yawnt <me@yawnt.com>
2015-07-14 15:21:51 +0200

    fix: enable more code

--- akka-js-actor/shared/src/main/scala/akka/actor/ActorSystem.scala
+++ akka-js-actor/shared/src/main/scala/akka/actor/ActorSystem.scala
@@ -367,12 +367,8 @@ abstract class ActorSystem extends ActorRefFactory {
   /**
    * Log the configuration.
    */
-/**
- * @note IMPLEMENT IN SCALA.JS
- *
-   def logConfiguration(): Unit
+  def logConfiguration(): Unit
 
- */
   /**
    * Construct a path below the application guardian to be used with [[ActorSystem.actorSelection]].
    */
@@ -595,11 +591,7 @@ abstract class ExtendedActorSystem extends ActorSystem {
    * Careful, this may OOM on large actor systems, and it is only meant for
    * helping debugging in case something already went terminally wrong.
    */
-/**
- * @note IMPLEMENT IN SCALA.JS
- *
    private[akka] def printTree: String
- */
 }
 
 private[akka] class ActorSystemImpl(val name: String, applicationConfig: ActorSystem.Settings /** @note IMPLEMENT IN SCALA.JS Config , classLoader: ClassLoader, defaultExecutionContext: Option[ExecutionContext] */) extends ExtendedActorSystem {
@@ -876,45 +868,41 @@ private[akka] class ActorSystemImpl(val name: String, applicationConfig: ActorSy
 
   override def toString: String = lookupRoot.path.root.address.toString
 
-/**
- * @note IMPLEMENT IN SCALA.JS
- *
-   override def printTree: String = {
-     def printNode(node: ActorRef, indent: String): String = {
-       node match {
-         case wc: ActorRefWithCell ⇒
-           val cell = wc.underlying
-           (if (indent.isEmpty) "-> " else indent.dropRight(1) + "⌊-> ") +
-             node.path.name + " " + Logging.simpleName(node) + " " +
-             (cell match {
-               case real: ActorCell ⇒ if (real.actor ne null) real.actor.getClass else "null"
-               case _               ⇒ Logging.simpleName(cell)
-             }) +
-             (cell match {
-               case real: ActorCell ⇒ " status=" + real.mailbox.currentStatus
-               case _               ⇒ ""
-             }) +
-             " " + (cell.childrenRefs match {
-             case ChildrenContainer.TerminatingChildrenContainer(_, toDie, reason) ⇒
-               "Terminating(" + reason + ")" +
-                 (toDie.toSeq.sorted mkString ("\n" + indent + "   |    toDie: ", "\n" + indent + "   |           ", ""))
-             case x @ (ChildrenContainer.TerminatedChildrenContainer | ChildrenContainer.EmptyChildrenContainer) ⇒ x.toString
-             case n: ChildrenContainer.NormalChildrenContainer ⇒ n.c.size + " children"
-             case x ⇒ Logging.simpleName(x)
-           }) +
-             (if (cell.childrenRefs.children.isEmpty) "" else "\n") +
-             ({
-               val children = cell.childrenRefs.children.toSeq.sorted
-               val bulk = children.dropRight(1) map (printNode(_, indent + "   |"))
-               bulk ++ (children.lastOption map (printNode(_, indent + "    ")))
-             } mkString ("\n"))
-         case _ ⇒
-           indent + node.path.name + " " + Logging.simpleName(node)
-       }
-     }
-     printNode(lookupRoot, "")
-   }
- */
+  override def printTree: String = {
+    def printNode(node: ActorRef, indent: String): String = {
+      node match {
+        case wc: ActorRefWithCell ⇒
+          val cell = wc.underlying
+          (if (indent.isEmpty) "-> " else indent.dropRight(1) + "⌊-> ") +
+            node.path.name + " " + Logging.simpleName(node) + " " +
+            (cell match {
+              case real: ActorCell ⇒ if (real.actor ne null) real.actor.getClass else "null"
+              case _               ⇒ Logging.simpleName(cell)
+            }) +
+            (cell match {
+              case real: ActorCell ⇒ " status=" + real.mailbox.currentStatus
+              case _               ⇒ ""
+            }) +
+            " " + (cell.childrenRefs match {
+            case ChildrenContainer.TerminatingChildrenContainer(_, toDie, reason) ⇒
+              "Terminating(" + reason + ")" +
+                (toDie.toSeq.sorted mkString ("\n" + indent + "   |    toDie: ", "\n" + indent + "   |           ", ""))
+            case x @ (ChildrenContainer.TerminatedChildrenContainer | ChildrenContainer.EmptyChildrenContainer) ⇒ x.toString
+            case n: ChildrenContainer.NormalChildrenContainer ⇒ n.c.size + " children"
+            case x ⇒ Logging.simpleName(x)
+          }) +
+            (if (cell.childrenRefs.children.isEmpty) "" else "\n") +
+            ({
+              val children = cell.childrenRefs.children.toSeq.sorted
+              val bulk = children.dropRight(1) map (printNode(_, indent + "   |"))
+              bulk ++ (children.lastOption map (printNode(_, indent + "    ")))
+            } mkString ("\n"))
+        case _ ⇒
+          indent + node.path.name + " " + Logging.simpleName(node)
+      }
+    }
+    printNode(lookupRoot, "")
+  }
 
   final class TerminationCallbacks extends Runnable /** @note IMPLEMENT IN SCALA.JS with Awaitable[Unit] */ {
     /** @note IMPLEMENT IN SCALA.JS

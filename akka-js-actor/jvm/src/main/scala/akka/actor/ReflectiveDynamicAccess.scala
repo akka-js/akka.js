08994104d40399a552240bb326ad24f13b48c8b3
Andrea <andrea.peruffo1982@gmail.com>
2015-08-12 22:27:31 +0200

    closer to next version of akka with splitted files

--- akka-js-actor/jvm/src/main/scala/akka/actor/ReflectiveDynamicAccess.scala
+++ akka-js-actor/jvm/src/main/scala/akka/actor/ReflectiveDynamicAccess.scala
@@ -9,52 +9,6 @@ import scala.reflect.ClassTag
 import scala.util.Try
 
 /**
- * The DynamicAccess implementation is the class which is used for
- * loading all configurable parts of an actor system (the
- * [[akka.actor.ReflectiveDynamicAccess]] is the default implementation).
- *
- * This is an internal facility and users are not expected to encounter it
- * unless they are extending Akka in ways which go beyond simple Extensions.
- */
-abstract class DynamicAccess {
-  /**
-   * Convenience method which given a `Class[_]` object and a constructor description
-   * will create a new instance of that class.
-   *
-   * {{{
-   * val obj = DynamicAccess.createInstanceFor(clazz, Seq(classOf[Config] -> config, classOf[String] -> name))
-   * }}}
-   */
-  def createInstanceFor[T: ClassTag](clazz: Class[_], args: immutable.Seq[(Class[_], AnyRef)]): Try[T]
-
-  /**
-   * Obtain a `Class[_]` object loaded with the right class loader (i.e. the one
-   * returned by `classLoader`).
-   */
-  def getClassFor[T: ClassTag](fqcn: String): Try[Class[_ <: T]]
-
-  /**
-   * Obtain an object conforming to the type T, which is expected to be
-   * instantiated from a class designated by the fully-qualified class name
-   * given, where the constructor is selected and invoked according to the
-   * `args` argument. The exact usage of args depends on which type is requested,
-   * see the relevant requesting code for details.
-   */
-  def createInstanceFor[T: ClassTag](fqcn: String, args: immutable.Seq[(Class[_], AnyRef)]): Try[T]
-
-  /**
-   * Obtain the Scala “object” instance for the given fully-qualified class name, if there is one.
-   */
-  def getObjectFor[T: ClassTag](fqcn: String): Try[T]
-
-  /**
-   * This is the class loader to be used in those special cases where the
-   * other factory method are not applicable (e.g. when constructing a ClassLoaderBinaryInputStream).
-   */
-  def classLoader: ClassLoader
-}
-
-/**
  * This is the default [[akka.actor.DynamicAccess]] implementation used by [[akka.actor.ExtendedActorSystem]]
  * unless overridden. It uses reflection to turn fully-qualified class names into `Class[_]` objects
  * and creates instances from there using `getDeclaredConstructor()` and invoking that. The class loader

a773294c71b9dcf4c99242f7239eb87e662ddeb7
andreaTP <andrea.peruffo1982@gmail.com>
2016-08-05 11:08:51 +0200

    cross-compile Attributes.scala

---
+++ akka-js-actor/js/src/main/scala/java/net/URLEncoder.scala
@@ -0,0 +1,16 @@
+package java.net
+
+import java.io.UnsupportedEncodingException
+
+import scala.scalajs.js
+
+object URLEncoder {
+
+  def encode(s: String, enc: String): String = {
+    if (enc != "UTF-8")
+      throw new UnsupportedEncodingException(enc)
+    else
+      js.Dynamic.global.encodeURI(s).asInstanceOf[String]
+  }
+
+}

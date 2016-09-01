3abfd78efd21821d84e31e29301fc9d0bc7c49e6
andreaTP <andrea.peruffo1982@gmail.com>
2016-09-01 14:57:49 +0200

    kix method visibility

--- akka-js-actor/js/src/main/scala/akka/dispatch/AbstractNodeQueue.scala
+++ akka-js-actor/js/src/main/scala/akka/dispatch/AbstractNodeQueue.scala
@@ -3,9 +3,9 @@ package akka.dispatch
 import scala.collection.mutable.Queue
 
 class AbstractNodeQueue[A >: Null] extends Queue[A] {
-  private[dispatch] def add(value: A) = super.enqueue(value)
+  def add(value: A) = super.enqueue(value)
 
-  private[dispatch] def poll(): A = if(isEmpty) null else super.dequeue()
+  def poll(): A = if(isEmpty) null else super.dequeue()
 
-  private[dispatch] def count() = size
+  def count() = size
 }

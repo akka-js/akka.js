fa2db4922362910be01cf5ab10dc73131855f419
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2017-12-14 14:44:43 +0000

    fixes for 2.5.8

--- akka-js-stream-tests/js/src/test/scala/akka/stream/scaladsl/Thread.scala
+++ akka-js-stream-tests/js/src/test/scala/akka/stream/scaladsl/Thread.scala
@@ -2,9 +2,12 @@ package akka.stream.scaladsl
 
 import akka.testkit.ThreadUtil
 import akka.actor.ActorSystem
+import java.{lang => jl}
 
 object Thread {
 
   def sleep(duration :Long)(implicit system: ActorSystem): Unit  = ThreadUtil.sleep(duration)(system)
+
+  def currentThread(): jl.Thread = jl.Thread.currentThread()
 
 }

f02fc45ebe1426faf08ac2674f7da4a95c148132
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2018-03-13 17:02:33 +0000

    distinct implementation of Future

---
+++ akka-js-typed-testkit/js/src/main/scala-2.11/akka/testkit/typed/scaladsl/Future.scala
@@ -0,0 +1,50 @@
+package akka.testkit.typed.scaladsl
+
+import scala.concurrent.duration._
+// fixing the hard way
+
+object Promise {
+
+  def apply[T]() = new WrappedPromise[T]{
+    val inner = scala.concurrent.Promise[T]()
+  }
+
+}
+
+trait WrappedPromise[T] extends scala.concurrent.Promise[T] {
+  self =>
+  val inner: scala.concurrent.Promise[T] 
+
+  lazy val future: WrappedFuture[T] = new WrappedFuture[T]{
+    val prom = self.inner
+  }
+
+  def isCompleted: Boolean =
+    inner.isCompleted
+  def tryComplete(result: scala.util.Try[T]): Boolean =
+    inner.tryComplete(result)
+}
+
+trait WrappedFuture[T] extends scala.concurrent.Future[T] {
+  self =>
+  val prom: scala.concurrent.Promise[T]
+  lazy val inner = prom.future
+
+  def futureValue(): T =
+    Await.result(inner, 5 seconds) // make it configurable? .asInstanceOf[T]
+
+  def ready(atMost: scala.concurrent.duration.Duration)(implicit permit: scala.concurrent.CanAwait): this.type = {
+    self.inner.ready(atMost)(permit)
+    this
+  }
+  def result(atMost: scala.concurrent.duration.Duration)(implicit permit: scala.concurrent.CanAwait): T =
+    inner.result(atMost)(permit)
+
+  def isCompleted: Boolean =
+    inner.isCompleted
+  def onComplete[U](f: scala.util.Try[T] => U)(implicit executor: scala.concurrent.ExecutionContext): Unit =
+    inner.onComplete[U](f)(executor)
+  def value: Option[scala.util.Try[T]] =
+    inner.value
+
+}

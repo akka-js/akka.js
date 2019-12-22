f7ad76180ccde47310064cc6bb119a52feb88fea
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2019-12-22 19:05:17 +0000

    disable 2.13

--- publish.sh
+++ publish.sh
@@ -6,6 +6,7 @@ sbt "akkaJsActorJS/compile"
 
 sbt ";++2.12.10;akkaJsActorJS/publishSigned;akkaJsActorStreamJS/publishSigned;akkaJsActorStreamTypedJS/publishSigned;akkaJsActorTypedJS/publishSigned;akkaJsStreamTestkitJS/publishSigned;akkaJsTestkitJS/publishSigned;akkaJsTypedTestkitJS/publishSigned"
 
-sbt ";++2.13.1;akkaJsActorJS/publishSigned;akkaJsActorStreamJS/publishSigned;akkaJsActorStreamTypedJS/publishSigned;akkaJsActorTypedJS/publishSigned;akkaJsStreamTestkitJS/publishSigned;akkaJsTestkitJS/publishSigned;akkaJsTypedTestkitJS/publishSigned"
+# temporary disable 2.13
+# sbt ";++2.13.1;akkaJsActorJS/publishSigned;akkaJsActorStreamJS/publishSigned;akkaJsActorStreamTypedJS/publishSigned;akkaJsActorTypedJS/publishSigned;akkaJsStreamTestkitJS/publishSigned;akkaJsTestkitJS/publishSigned;akkaJsTypedTestkitJS/publishSigned"
 
 sbt sonatypeReleaseAll
